5bcc06bf9d5e291dd0822a13da6a674b43c142fa
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-08 18:41:59 +0500

    feat(Main): added Numeric Literals with underscore just for readiblity

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -3,15 +3,7 @@ package com.mirkamolcode;
 public class Main {
 
      static void main(String[] args) {
-       // primitives
-         int number1 = 20;
-         int number2 = 20;
-         int result = number1 + number2;
-         System.out.println(result);
-         double pi = 3.14;
-         boolean isAdult = true;
-         char a = 'A';
-         int n = 330000;
-         System.out.println(pi);
+         int amount = 1000_000_000; 
+         System.out.println(amount); // the outp is 1000000000
      }
 }

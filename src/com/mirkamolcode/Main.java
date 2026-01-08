e9ee06ef9aafc5476f34c7716454961c5fc736f4
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-08 23:57:15 +0500

    feat(Main): added reference types and Objects

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -1,18 +1,15 @@
 package com.mirkamolcode;
 
+import java.awt.*;
+
 public class Main {
 
     static void main(String[] args) {
-        String amigos = "Amigos";
-        String code = "code";
-        String brand = amigos + code;
-        System.out.println(brand.toUpperCase()); // AMIGOSCODE
-        System.out.println(brand.toLowerCase()); // amigoscode
-        System.out.println(brand.substring(6)); // code
-        System.out.println(brand.substring(0,6)); // Amigos
-        System.out.println("   ".isEmpty()); // fasle
-        System.out.println("    ".isBlank()); // true, there is no text
-        System.out.println("   a    "); //    a
-        System.out.println("   a    ".trim()); // a, removes spaces
+       // reference Types / Objects
+       int age = 21;
+        Point pointA = new Point(10, 10);
+        System.out.println("pointA = " + pointA);
+        pointA.move(10,11);
+        System.out.println("pointA = " + pointA);
     }
 }

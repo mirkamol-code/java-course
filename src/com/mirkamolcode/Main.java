39eaa3224ae48217aff1f88143b6ab27f6230f86
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-08 18:59:36 +0500

    feat(Main): added increment and decrement operators

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -3,15 +3,26 @@ package com.mirkamolcode;
 public class Main {
 
     static void main(String[] args) {
-        int numberOne = 10;
-        int numberTwo = 3;
+        int number = 0;
 
-        System.out.println(numberOne + numberTwo);
-        System.out.println(numberOne - numberTwo);
-        System.out.println(numberOne * numberTwo);
-        System.out.println(numberOne / numberTwo);
-        System.out.println(numberOne % numberTwo);
+        // decrement operator: --
+        System.out.println(number--); // the output will be 0
+        System.out.println(number); // the output will be -1
+        int numberTwo = 0;
+        System.out.println(--numberTwo); // the output will be -1
+        System.out.println(numberTwo); // the output will be -1
 
+        // increment operator: ++
+        System.out.println(number++); // the output will be 0
+        System.out.println(number); // the output will be 1
+        int numberThree = 0;
+        System.out.println(++numberThree); // the output will be 1
+        System.out.println(numberThree); // the output will be 1
 
+        int number2 = 0;
+        int incremet = number2 + 1;
+        int decremet = number2 - 1;
+        System.out.println(incremet); // the output will be 1
+        System.out.println(decremet); // the output will be -1
     }
 }

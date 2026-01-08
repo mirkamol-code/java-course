8b4ef7c87b6b5068d98ec0dfe69e406a9a557608
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-08 22:56:46 +0500

    feat(Main): added strings and its methods

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -3,17 +3,16 @@ package com.mirkamolcode;
 public class Main {
 
     static void main(String[] args) {
-      // Comparison operators
-      // <, <=, >, >=, ==, !=
-        System.out.println(10 > 10); // the output will be false
-        System.out.println(10 >= 10); // the output will be true
-        System.out.println(10 < 10); // the output will be false
-        System.out.println(10 <= 10); // the output will be true
-        System.out.println(10 == 10); // the output will be true
-        System.out.println(10 != 10); // the output will be false
-
-        System.out.println();
-        boolean isAdult = 15 >= 16;
-        System.out.println(isAdult);
+        String amigos = "Amigos";
+        String code = "code";
+        String brand = amigos + code;
+        System.out.println(brand.toUpperCase()); // AMIGOSCODE
+        System.out.println(brand.toLowerCase()); // amigoscode
+        System.out.println(brand.substring(6)); // code
+        System.out.println(brand.substring(0,6)); // Amigos
+        System.out.println("   ".isEmpty()); // fasle
+        System.out.println("    ".isBlank()); // true, there is no text
+        System.out.println("   a    "); //    a
+        System.out.println("   a    ".trim()); // a, removes spaces
     }
 }

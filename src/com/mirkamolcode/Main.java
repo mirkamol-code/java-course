0a125310cd1bbbd4309804b9ec30d7247f63396c
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-08 20:20:24 +0500

    feat(Main): added Comparison operators and booleans

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -3,26 +3,17 @@ package com.mirkamolcode;
 public class Main {
 
     static void main(String[] args) {
-        int number = 0;
+      // Comparison operators
+      // <, <=, >, >=, ==, !=
+        System.out.println(10 > 10); // the output will be false
+        System.out.println(10 >= 10); // the output will be true
+        System.out.println(10 < 10); // the output will be false
+        System.out.println(10 <= 10); // the output will be true
+        System.out.println(10 == 10); // the output will be true
+        System.out.println(10 != 10); // the output will be false
 
-        // decrement operator: --
-        System.out.println(number--); // the output will be 0
-        System.out.println(number); // the output will be -1
-        int numberTwo = 0;
-        System.out.println(--numberTwo); // the output will be -1
-        System.out.println(numberTwo); // the output will be -1
-
-        // increment operator: ++
-        System.out.println(number++); // the output will be 0
-        System.out.println(number); // the output will be 1
-        int numberThree = 0;
-        System.out.println(++numberThree); // the output will be 1
-        System.out.println(numberThree); // the output will be 1
-
-        int number2 = 0;
-        int incremet = number2 + 1;
-        int decremet = number2 - 1;
-        System.out.println(incremet); // the output will be 1
-        System.out.println(decremet); // the output will be -1
+        System.out.println();
+        boolean isAdult = 15 >= 16;
+        System.out.println(isAdult);
     }
 }

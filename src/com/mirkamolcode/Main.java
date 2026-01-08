fe51d8f5b3460ecd12bc12d455f07052ad296b9b
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-09 01:17:56 +0500

    feat(Main): solved Task 2

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -6,7 +6,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I can compile and run from the terminal. Hooray 🎉");
+        System.out.println("I love cats");
+//        System.out.println("I love cats");
     }
 }
 

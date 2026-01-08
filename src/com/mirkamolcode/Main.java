f1150846e8f4f760bc624f8941487c5346358f31
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-09 01:12:13 +0500

    feat(Main): solved Task 1

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -5,22 +5,8 @@ import java.util.Arrays;
 
 public class Main {
 
-    static void main(String[] args) {
-        // Arrays
-        int [] numbers = new int[3];
-        numbers[0] = 1;
-        numbers[1] = 33;
-        numbers[2] = 6;
-        System.out.println(
-                Arrays.toString(numbers)
-        ); //[1, 33, 6]
-        System.out.println(numbers); // [I@2a84aee7
-        System.out.println(numbers.length); // 3
-
-        int [] numbers2 = {0, 5, 6, 8, 9};
-        System.out.println(numbers2.length); // 5
-
-        String [] names = {"Jamila", "Alex"};
-        System.out.println(Arrays.toString(names)); // [Jamila, Alex]
+    public static void main(String[] args) {
+        System.out.println("I can compile and run from the terminal. Hooray 🎉");
     }
 }
+

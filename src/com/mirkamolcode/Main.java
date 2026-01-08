900b743db266f35bf6d0d2026617adc5fffe99b0
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-09 01:34:44 +0500

    feat(Main): solved Task 4
     // Write a program that transform "Amigoscode" to "AMIGOSCODE"

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -1,25 +1,16 @@
 package com.mirkamolcode;
 
-import java.awt.*;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-     // Create variables to store the strings and the result
-        String string1 = "Hello";
-        String string2 = "World";
-        String result;
+        String str = "Amigoscode";
+        toUpperCase(str);
+    }
 
-        // Compare the strings using the equals method
-        if (string1.equals(string2)) {
-            result = "Both strings are equal.";
-        } else {
-            result = "Both strings are not equal.";
-        }
-
-        // Print the result
-        System.out.println(result);
+    public static void toUpperCase(String lowerCase) {
+        String upperCase = lowerCase.toUpperCase();
+        System.out.println(upperCase);
     }
 }
 

be204bce5befdded1856169ae38f70a55035a1f7
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-08 18:30:22 +0500

    feat(Main): added Primitive Data types

--- src/com/mirkamolcode/Main.java
+++ src/com/mirkamolcode/Main.java
@@ -1,25 +1,17 @@
 package com.mirkamolcode;
-/* This is public keyword: an access modifier
-that allows method to be accessible from anywhere
- */
+
 public class Main {
-    /*
-        1. Static: Indicates that the method belongs to the class, rather than instances of the class.
-        This means it can be called without creating an object of the class
-        2. void: specifies that the method does not return any value.
-        3. main: This is the entry point of any Java program.
-        The Java Virtual Machine (JVM) looks for this method to start execution.
-        4. The public static void main(String[] args) method is the entry point of any Java application.
-        When you run a Java program, the JVM calls the main method to start the execution of the program.
-        The String[] args parameter is used to pass command-line arguments to the program.
-   */
+
      static void main(String[] args) {
-         // this is comment
-         /* this is a multiline comment
-         Multi
-         line
-         code
-          */
-        System.out.println("Hello Java");
+       // primitives
+         int number1 = 20;
+         int number2 = 20;
+         int result = number1 + number2;
+         System.out.println(result);
+         double pi = 3.14;
+         boolean isAdult = true;
+         char a = 'A';
+         int n = 330000;
+         System.out.println(pi);
      }
 }
